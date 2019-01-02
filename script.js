4dbe0f82ded718910f0dc9a4a5d24f3466de84ef
Oleg Zhylin <o.zhilin@yahoo.com>
2019-01-03 00:18:27 +0200

    ff

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Hello');
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition((pos)=>{
+  	console.log('pos', pos);
+  },
+  (err)=>{
+  	console.log('err', err);
+  }
+  	);
+
+}
