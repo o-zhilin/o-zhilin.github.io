84c5e394a2ee1849030973694783ea177e9ec756
Oleg Zhylin <o.zhilin@yahoo.com>
2019-01-03 00:30:52 +0200

    ff

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition((pos)=>{
-  	console.log('pos', pos);
+  	console.log('pos', pos.coords);
   	alert(JSON.stringify(pos));
   	document.write(JSON.stringify(pos));
   },
