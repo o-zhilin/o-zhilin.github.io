1d19c3fd887f0e309042dc1ca4ce2c3116400374
Олег <o.zhilin@yahoo.com>
2022-01-16 16:51:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition((pos)=>{
-  	alert('lat: '+ pos.coords.latitude + ' long: ' + pos.coords.longitude);
-  },
-  (err)=>{
-  	console.log('err', err);
-  }
-  	);
-
-}
+console.log('Hello JS.');
